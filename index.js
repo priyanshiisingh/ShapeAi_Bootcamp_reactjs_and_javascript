9a3075f3529942dbcc467f2a696ee5f3a2617c43
priyanshiisingh <87604551+priyanshiisingh@users.noreply.github.com>
2021-07-30 11:53:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
